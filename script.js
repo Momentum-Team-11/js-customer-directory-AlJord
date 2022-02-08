5e163d74ef34240c9a3ad26a32e4d06607a9a871
Alex Jordan <AlJord91@gmail.com>
2022-02-07 21:48:05 -0500

    first names w/ pics only

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 console.log(customers)
 
 const customer = customers [0]
-const customersDiv = document.getElementById('customers')
-
+const customersDiv = document.getElementById('customers');
+console.log(customersDiv)
+for( let customer of customers) {
 let names = customer.name
 console.log('Name: ', names)
 
@@ -11,11 +12,21 @@ const h2El = document.createElement('h2')
 h2El.classList.add('Customers-Heading')
 console.log(h2El)
 
-const h2Text = document.createTextNode(names)
+const h2Text = document.createTextNode(names.first)
+console.log(h2Text)
 
 h2El.appendChild(h2Text)
 
 customersDiv.appendChild(h2El)
 
-let srcURL = customers.picture
+let srcURL = customer.picture.large
 console.log('src is ', srcURL)
+
+let customerPicture = document.createElement('img')
+
+customersDiv.innerHTML += `
+<div>
+    <img class='customer-img' src=${srcURL} />
+    </div>
+    `
+}
